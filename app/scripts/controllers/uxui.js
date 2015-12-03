3dda4894d56eb0ec3ddec046eb45ce5267eb63fe
Rick G <lacunainc04@gmail.com>
2015-12-02 19:37:57 -0800

    Add style in head on index.html to override default styles and add add Swiper initialization on the template All of this was done to get figure out why the Swiper was not being loaded. The way that the DOM is being manipulated is the issue.

--- app/scripts/controllers/uxui.js
+++ app/scripts/controllers/uxui.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 /**
@@ -9,33 +10,10 @@
  */
 angular.module('dinpApp')
   .controller('UxuiCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
    $http.get('http://newbiecoderwarehouse.github.io/design-inspiration-api/apis/uxUi.json').success(function(theapi) {
     $scope.myapi = theapi.results;
    });
 
+   });
 
-    var galleryTop = new Swiper('.gallery-top', {
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        spaceBetween: 10,
-        keyboardControl: true
-    });
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-        spaceBetween: 10,
-        centeredSlides: true,
-        slidesPerView: 'auto',
-        touchRatio: 0.2,
-        slideToClickedSlide: true
-    });
-    galleryTop.params.control = galleryThumbs;
-    galleryThumbs.params.control = galleryTop;
-
-     var swipers = $(".angular-slide").length;
-    alert("Num of swiper slides: " + swipers);
-  });
