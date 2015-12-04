c0f43595c71ae72b26cf2a7cea257af1656f2691
Rick G <lacunainc04@gmail.com>
2015-12-04 03:16:10 -0800

    Implement swiperCard directive on main image

--- app/scripts/controllers/typography.js
+++ app/scripts/controllers/typography.js
@@ -11,27 +11,8 @@ angular.module('dinpApp')
   .controller('TypographyCtrl', function ($scope, $http) {
     $http.get('http://newbiecoderwarehouse.github.io/design-inspiration-api/apis/typography.json').success(function(theapi, response) {
     $scope.myapi = theapi.results;
-    console.log(response);
-   	});
-    $(window).load(function(){
-        var galleryTop = new Swiper('.gallery-top', {
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        spaceBetween: 10,
-        keyboardControl: true
-    	});
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-        spaceBetween: 10,
-        centeredSlides: true,
-        slidesPerView: 'auto',
-        touchRatio: 0.2,
-        slideToClickedSlide: true
-    });
-    galleryTop.params.control = galleryThumbs;
-    galleryThumbs.params.control = galleryTop;
     
-
-    });
+   	});
 
 
 
