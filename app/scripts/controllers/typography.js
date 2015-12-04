e9006b9cdf13f56052a87d78ae91a2367120be90
Rick G <lacunainc04@gmail.com>
2015-12-04 00:15:02 -0800

    Fix main gallery images

--- app/scripts/controllers/typography.js
+++ app/scripts/controllers/typography.js
@@ -8,10 +8,31 @@
  * Controller of the dinpApp
  */
 angular.module('dinpApp')
-  .controller('TypographyCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('TypographyCtrl', function ($scope, $http) {
+    $http.get('http://newbiecoderwarehouse.github.io/design-inspiration-api/apis/typography.json').success(function(theapi, response) {
+    $scope.myapi = theapi.results;
+    console.log(response);
+   	});
+    $(window).load(function(){
+        var galleryTop = new Swiper('.gallery-top', {
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 10,
+        keyboardControl: true
+    	});
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        touchRatio: 0.2,
+        slideToClickedSlide: true
+    });
+    galleryTop.params.control = galleryThumbs;
+    galleryThumbs.params.control = galleryTop;
+    
+
+    });
+
+
+
   });
