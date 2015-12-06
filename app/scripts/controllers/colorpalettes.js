342827927bdcdde9cf34defbd8c54a2495991832
antonija_simic <antonija1023@gmail.com>
2015-12-06 15:10:49 +0100

    add slideshow to all pages but codesnippets

--- app/scripts/controllers/colorpalettes.js
+++ app/scripts/controllers/colorpalettes.js
@@ -7,11 +7,16 @@
  * # ColorpalettesCtrl
  * Controller of the dinpApp
  */
-angular.module('dinpApp')
-  .controller('ColorpalettesCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+ angular.module('dinpApp')
+ .controller('ColorpalettesCtrl', function ($scope, $http) {
+  $scope.awesomeThings = [
+  'HTML5 Boilerplate',
+  'AngularJS',
+  'Karma'
+  ];
+
+  $http.get('http://newbiecoderwarehouse.github.io/design-inspiration-api/apis/colorPalettes.json').success(function(theapi) {
+    $scope.myapi = theapi.results;
   });
+
+});
