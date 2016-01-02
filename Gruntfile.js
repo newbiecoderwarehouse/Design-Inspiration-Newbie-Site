84bd2f0bb5e7615ebfee02f969903943fcf6d3a9
antonija_simic <antonija1023@gmail.com>
2016-01-02 17:25:39 +0100

    add new layout; add new sidebar;

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,7 +66,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9001,
+        port: 9002,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
         livereload: 35729
@@ -92,7 +92,7 @@ module.exports = function (grunt) {
       },
       test: {
         options: {
-          port: 9001,
+          port: 9002,
           middleware: function (connect) {
             return [
               connect.static('.tmp'),
